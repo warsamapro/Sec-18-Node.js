0e2f8701748f58fe26e0f5dd6fd2d3ff470b6420
warsamapro <warsama.moussa@yahoo.ca>
2020-05-30 03:57:41 -0600

    first commit

---
+++ node.js
@@ -0,0 +1,10 @@
+// jshint eversion:6
+
+var superheroes = require('superheroes');
+var supervillains = require('supervillains');
+
+var mySupervillainsName = supervillains.random();
+var mySuperheroesName = superheroes.random();
+
+console.log(mySupervillainsName);
+console.log(mySuperheroesName);
